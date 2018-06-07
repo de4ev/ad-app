01af2d8ffb2c40bbc27b6878ba4cc5149cc58c38
Ivan <decev.v@gmail.com>
2018-06-07 23:59:41 +0300

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,42 @@
 <template>
- <h1>Hello Vue</h1>
+  <v-app>
+    <v-navigation-drawer app temporary
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>star</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Link 1</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon
+        @click="drawer = !drawer"
+        >
+      </v-toolbar-side-icon>
+      <v-toolbar-title>Ad application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
